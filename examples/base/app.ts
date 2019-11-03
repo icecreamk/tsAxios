e07f79184260e16f60108eb236ebfd96fa1f8e72
kkxxll <550893685@qq.com>
2019-11-03 15:02:57 +0800

    添加响应处理、添加promise

--- examples/base/app.ts
+++ examples/base/app.ts
@@ -99,3 +99,32 @@ axios({
   url: '/base/post',
   data: searchParams
 })
+
+axios({
+  method: 'post',
+  url: '/base/post',
+  headers: {
+    'content-type': 'application/json;charset=utf-8'
+  },
+  data: {
+    a: 1,
+    b: 2
+  }
+}).then((res) => {
+  console.log(res)
+})
+
+axios({
+  method: 'post',
+  url: '/base/post',
+  headers: {
+    'content-type': 'application/json;charset=utf-8'
+  },
+  responseType: 'json',
+  data: {
+    a: 3,
+    b: 4
+  }
+}).then((res) => {
+  console.log(res)
+})
