6f8da8ced7fa9928a55fbc218b1529c8bb06539b
kkxxll <550893685@qq.com>
2019-11-03 10:19:10 +0800

    添加入口文件

---
+++ src/types/index.ts
@@ -0,0 +1,14 @@
+export type Mathod = 'get' | 'GET'
+| 'delete' | 'DELETE'
+| 'head' | 'HEAD'
+| 'options' | 'OPTIONS'
+| 'post' | 'POST'
+| 'put' | 'PUT'
+| 'patch' | 'PATCH'
+
+export interface AxiosRequestConfig {
+  url: string
+  method?: Mathod
+  data?: any
+  params?: any
+}
