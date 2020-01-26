b8be56f81696700fbf734d91742c9723360cae7e
kkxxll <550893685@qq.com>
2020-01-26 09:54:07 +0800

    支持跨域（cookie）

---
+++ examples/server2.js
@@ -0,0 +1,35 @@
+// 用于测试跨域 对应example/more
+const express = require('express')
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+
+const app = express()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cookieParser())
+
+const router = express.Router()
+
+// 接收http://localhost:8080的cookie访问
+const cors = {
+  'Access-Control-Allow-Origin': 'http://localhost:8080',
+  'Access-Control-Allow-Credentials': true,
+  'Access-Control-Allow-Methods': 'POST, GET, PUT, DELETE, OPTIONS',
+  'Access-Control-Allow-Headers': 'Content-Type'
+}
+
+router.post('/more/server2', function(req, res) {
+  res.set(cors)
+  res.json(req.cookies)
+})
+
+router.options('/more/server2', function(req, res) {
+  res.set(cors)
+  res.end()
+})
+
+app.use(router)
+
+const port = 8088
+module.exports = app.listen(port)
